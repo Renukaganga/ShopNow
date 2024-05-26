211aa337a73a2b7aa3de9fcb9f799a569c848bd9
Renukaganga <22b01a1231@svecw.edu.in>
2024-05-26 13:34:34 +0530

    first

--- app/(front)/product/[slug]/page.tsx
+++ app/(front)/product/[slug]/page.tsx
@@ -1,16 +1,30 @@
+import AddToCart from '@/components/products/AddToCart'
 
-import data from '@/lib/data'
 import Image from 'next/image'
 import Link from 'next/link'
 
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await productService.getBySlug(params.slug)
+  if (!product) {
+    return { title: 'Product not found' }
+  }
+  return {
+    title: product.name,
+    description: product.description,
+  }
+}
 
 export default async function ProductDetails({
   params,
 }: {
   params: { slug: string }
 }) {
-  const product=data.products.find((x)=> x.slug === params.slug)
+  const product = await productService.getBySlug(params.slug)
   if (!product) {
     return <div>Product not found</div>
   }
@@ -21,7 +35,6 @@ export default async function ProductDetails({
       </div>
       <div className="grid md:grid-cols-4 md:gap-3">
         <div className="md:col-span-2">
-
           <Image
             src={product.image}
             alt={product.name}
@@ -39,8 +52,12 @@ export default async function ProductDetails({
             <li>
               <h1 className="text-xl">{product.name}</h1>
             </li>
-            
-          
+            <li>
+              <Rating
+                value={product.rating}
+                caption={`${product.numReviews} ratings`}
+              />
+            </li>
             <li> {product.brand}</li>
             <li>
               <div className="divider"></div>
@@ -63,8 +80,19 @@ export default async function ProductDetails({
                   {product.countInStock > 0 ? 'In stock' : 'Unavailable'}
                 </div>
               </div>
-              
-          </div>
+              {product.countInStock !== 0 && (
+                <div className="card-actions justify-center">
+                  <AddToCart
+                    item={{
+                      ...convertDocToObj(product),
+                      qty: 0,
+                      color: '',
+                      size: '',
+                    }}
+                  />
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
