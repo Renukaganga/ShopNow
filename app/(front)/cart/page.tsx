4abd7dcfc2c29c9a2b56954aa2ed1d49a2d2ecd4
Renukaganga <22b01a1231@svecw.edu.in>
2024-05-27 19:32:16 +0530

    Add-to-cart

--- app/(front)/cart/page.tsx
+++ app/(front)/cart/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import CartDetails from './CartDetails'
+
+export const metadata = {
+  title: 'Shopping Cart',
+}
 
 const CartPage = () => {
-  return (
-    <div>
-      page
-    </div>
-  )
+  return <CartDetails />
 }
 
 export default CartPage
